efd7341c628c5ab411147b2d38336d42f8fb6929
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-10-15 14:26:31 -0700

    Updates

---
+++ precache-manifest.5db93a1a5d6341c4ba86563d760449b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441bee267e0183595f73aa0800693e5b",
+    "url": "/Namecard/index.html"
+  },
+  {
+    "revision": "a4c405cc663d541fad1d",
+    "url": "/Namecard/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "75ea7f602696c3769e8e",
+    "url": "/Namecard/static/css/main.5e73c9a2.chunk.css"
+  },
+  {
+    "revision": "a4c405cc663d541fad1d",
+    "url": "/Namecard/static/js/2.52355532.chunk.js"
+  },
+  {
+    "revision": "75ea7f602696c3769e8e",
+    "url": "/Namecard/static/js/main.04275cde.chunk.js"
+  },
+  {
+    "revision": "7987cbc9bf636d8f27a2",
+    "url": "/Namecard/static/js/runtime~main.f5861dc5.js"
+  }
+]);
