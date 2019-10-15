6757e0bac301a7bc46a9062287ef5bba2998eb1b
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-10-15 14:20:48 -0700

    Creating Name Card using react components

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Manideep Kamarajugadda',
+    username: 'deep8mani',
+    email: 'deep8mani@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Surya Vamsee Maddukuri',
+    username: 'Gaali Ninja',
+    email: 'GaaliNinja@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'pavan kumar ',
+    username: 'pataaspavan',
+    email: 'pataaspavan@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Arun Reddy',
+    username: '2rupees',
+    email: 'reddy@vallapureddy.com'
+  },
+  {
+    id: 5,
+    name: 'Yeshwanth Kumar',
+    username: 'Yesh Boy',
+    email: 'yeshwanthreddy@ankamreddi.com'
+  },
+  {
+    id: 6,
+    name: 'Srikanth Reddy',
+    username: 'Nagidi',
+    email: 'srikanthreddy@nagidi.com'
+  },
+  {
+    id: 7,
+    name: 'Sandeep',
+    username: 'Sandeep',
+    email: 'Sandeep@alla.com'
+  },
+  {
+    id: 8,
+    name: 'Gayathri',
+    username: 'Gayathri',
+    email: 'Gayathri@sainikomu.com'
+  },
+  {
+    id: 9,
+    name: 'Parasuram ',
+    username: 'SP Parasuram',
+    email: 'Parasuram@reddy.com'
+  },
+  {
+    id: 10,
+    name: 'Sai Varun Tangalla',
+    username: 'VarunTangalla',
+    email: 'VarunTangalla@gmail.com'
+  },
+];
